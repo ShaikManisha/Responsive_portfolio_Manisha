2d45555b05f35f3d25a706c53fc7bfd9c36a5fed
Shaik Manisha <manishashaik0606@gmail.com>
2024-01-09 18:37:42 +0530

    removed errors

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import "../assets/scss/components/works.scss";
-import Work1 from "../assets/img/Work1.jpg";
-import Work2 from "../assets/img/Work2.jpg";
-import Work3 from "../assets/img/Work3.jpg";
-import Work4 from "../assets/img/Work4.jpg";
-import Work5 from "../assets/img/Work5.jpg";
+import Work1 from "../assets/img/work1.jpg";
+import Work2 from "../assets/img/work2.jpg";
+import Work3 from "../assets/img/work3.jpg";
+import Work4 from "../assets/img/work4.jpg";
+import Work5 from "../assets/img/work5.jpg";
 const workItems = [
   {
     id: 1,
